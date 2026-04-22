96ff31239b2bd312034153de34d6da5093235d16
Pujitha-2307 <24wh1a0597@bvrithyderabad.edu.in>
2026-04-22 22:17:34 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,49 +1,22 @@
 import java.util.Scanner;
 
-public class SimpleCalculator {
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter first number:");
         double num1 = sc.nextDouble();
-
-        System.out.println("Enter operator (+, -, *, /):");
         char operator = sc.next().charAt(0);
-
-        System.out.println("Enter second number:");
         double num2 = sc.nextDouble();
 
-        double result;
+        double result = 0;
 
         switch (operator) {
-            case '+':
-                result = num1 + num2;
-                System.out.println("Result: " + result);
-                break;
-
-            case '-':
-                result = num1 - num2;
-                System.out.println("Result: " + result);
-                break;
-
-            case '*':
-                result = num1 * num2;
-                System.out.println("Result: " + result);
-                break;
-
-            case '/':
-                if (num2 != 0) {
-                    result = num1 / num2;
-                    System.out.println("Result: " + result);
-                } else {
-                    System.out.println("Error: Cannot divide by zero");
-                }
-                break;
-
-            default:
-                System.out.println("Invalid operator");
+            case '+': result = num1 + num2; break;
+            case '-': result = num1 - num2; break;
+            case '*': result = num1 * num2; break;
+            case '/': result = num2 != 0 ? num1 / num2 : 0; break;
         }
 
-        sc.close();
+        System.out.print((int) result);
     }
 }
